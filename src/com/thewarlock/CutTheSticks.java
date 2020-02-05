8213cf9b4893047922783f937cd85f0157bd74c5
DWarlock <d.thewarlock@gmail.com>
2020-02-06 00:58:06 +0530

    Cut The Sticks Problem DONE

--- src/com/thewarlock/CutTheSticks.java
+++ src/com/thewarlock/CutTheSticks.java
@@ -1,9 +1,11 @@
+package com.thewarlock;
+
 import java.util.Arrays;
 
 public class CutTheSticks {
 
     static int[] cutTheSticks(int[] arr) {
-        StringBuffer str= new StringBuffer("");
+        /*StringBuffer str= new StringBuffer("");
         Arrays.sort(arr);
         int size=1;
         for(int i=1;i<arr.length;i++){
@@ -15,13 +17,37 @@ public class CutTheSticks {
         for(int i=0;i<arr.length;i++)
             str.append(arr[i]);
         System.out.println(str);
-        return arr;
+        return arr;*/
+        Arrays.sort(arr);
+        int size = 1;
+        int max = Arrays.stream(arr).max().getAsInt();
+        int ar[] = new int[max + 1];
+        int count = 0, sum = 0;
+        for (int i = 0; i < arr.length; i++)
+            ar[arr[i]]++;
+        for (int i = 0; i < ar.length; i++) {
+            if (ar[i] != 0)
+                count++;
+            sum += ar[i];
+        }
+
+        int result[] = new int[count];
+        result[0] = sum;
+        int j=1;
+        for (int i = 1; i < ar.length-1; i++) {
+            if (ar[i] != 0) {
+                sum -= ar[i];
+                result[j] = sum;
+                j++;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,3,3,2,1};
-        int ar[]=cutTheSticks(arr);
-        for(int i=0;i<ar.length;i++)
+        int arr[] = {5, 4, 4, 2, 2, 8};
+        int ar[] = cutTheSticks(arr);
+        for (int i = 0; i < ar.length; i++)
             System.out.println(ar[i]);
     }
 }
