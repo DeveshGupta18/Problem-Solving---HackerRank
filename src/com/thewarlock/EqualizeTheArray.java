b0cbaa61c2025e8929933f559fbe974f10dfb6d6
DWarlock <d.thewarlock@gmail.com>
2020-02-21 09:20:21 +0530

    Equalize The Array Problem DONE

--- src/com/thewarlock/EqualizeTheArray.java
+++ src/com/thewarlock/EqualizeTheArray.java
@@ -1,4 +1,48 @@
 package com.thewarlock;
 
+import java.io.*;
+import java.util.*;
+
 public class EqualizeTheArray {
+
+    // Complete the equalizeArray function below.
+    static int equalizeArray(int[] arr) {
+        Arrays.sort(arr);
+        int max = Arrays.stream(arr).max().getAsInt();
+        int ar[] =new int[max+1];
+        for(int i=0;i<arr.length;i++)
+        {
+            ar[arr[i]]++;
+        }
+        max = Arrays.stream(ar).max().getAsInt();
+        return arr.length-max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
