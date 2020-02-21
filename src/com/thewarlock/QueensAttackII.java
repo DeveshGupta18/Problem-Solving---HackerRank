ba555ebd45c5b1121439316e4029acc1f3ff6008
DWarlock <d.thewarlock@gmail.com>
2020-02-21 09:31:39 +0530

    Queens Attack II NOT DONE

--- src/com/thewarlock/QueensAttackII.java
+++ src/com/thewarlock/QueensAttackII.java
@@ -1,4 +1,12 @@
 package com.thewarlock;
 
 public class QueensAttackII {
+    static void queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles) {
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
