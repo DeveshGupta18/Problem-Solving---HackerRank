2645bbf0b823b3330ff5fbd02eb00e3d365024ea
DWarlock <d.thewarlock@gmail.com>
2020-02-21 09:22:38 +0530

    Halloween Sale Problem DONE

--- src/com/thewarlock/HalloweenSale.java
+++ src/com/thewarlock/HalloweenSale.java
@@ -1,4 +1,23 @@
 package com.thewarlock;
 
 public class HalloweenSale {
+    static int howManyGames(int p, int d, int m, int s) {
+        // Return the number of games you can buy
+        int count=0;
+        int sum=p;
+        while(sum<=s){
+                p-=d;
+            if (p>m){
+                sum+=p;
+            }
+            else
+                sum+=m;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(howManyGames(16, 2,1,9981));
+    }
 }
