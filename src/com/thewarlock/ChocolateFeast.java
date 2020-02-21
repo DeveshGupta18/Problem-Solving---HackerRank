54d1729b6fbd9ce3b861186a2f623efcde382cf9
DWarlock <d.thewarlock@gmail.com>
2020-02-21 09:19:39 +0530

    Chocolate Feast Problem DONE

--- src/com/thewarlock/ChocolateFeast.java
+++ src/com/thewarlock/ChocolateFeast.java
@@ -1,4 +1,17 @@
 package com.thewarlock;
 
 public class ChocolateFeast {
+    static int chocolateFeast(int n, int c, int m) {
+        int w = n / c;
+        int t = w;
+        while (w >= m) {
+            t += w/m;
+            w = w/m + w%m;
+        }
+        return t;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(chocolateFeast(6, 2, 2));
+    }
 }
