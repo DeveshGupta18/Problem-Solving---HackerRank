35279f1fdd5590223cb7bd1a4316dfc0f9336b4a
DWarlock <d.thewarlock@gmail.com>
2020-02-21 09:23:30 +0530

    Jumping On The Clouds: Revisited Problem DONE

--- src/com/thewarlock/JumpingOnTheClouds_Revisited.java
+++ src/com/thewarlock/JumpingOnTheClouds_Revisited.java
@@ -1,4 +1,22 @@
 package com.thewarlock;
 
 public class JumpingOnTheClouds_Revisited {
+    static int jumpingOnClouds(int[] c, int k) {
+        int n=c.length;
+        int i=k%n;
+        int e=100;
+        while(true){
+            e-=1;
+            if(c[i]==1)
+                e-=2;
+            if(i==0)
+                break;
+            i=(i+k)%n;
+        }
+        return e;
+    }
+    public static void main(String[] args) {
+        int c[]={0,0,1,0,0,1,1,0};
+        System.out.println(jumpingOnClouds(c, 2));
+    }
 }
