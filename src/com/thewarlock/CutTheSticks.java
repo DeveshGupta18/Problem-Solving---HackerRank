e15a302d56ad0bf23b64a15a4069b3e2a5bb139f
DWarlock <d.thewarlock@gmail.com>
2020-02-21 09:27:48 +0530

    Cut The Sticks Problem DONE

--- src/com/thewarlock/CutTheSticks.java
+++ src/com/thewarlock/CutTheSticks.java
@@ -5,19 +5,7 @@ import java.util.Arrays;
 public class CutTheSticks {
 
     static int[] cutTheSticks(int[] arr) {
-        /*StringBuffer str= new StringBuffer("");
-        Arrays.sort(arr);
-        int size=1;
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]!=arr[i-1])
-                size++;
-        }
-        int ar[] = new int[size];
-        ar[0]=arr.length;
-        for(int i=0;i<arr.length;i++)
-            str.append(arr[i]);
-        System.out.println(str);
-        return arr;*/
+
         Arrays.sort(arr);
         int size = 1;
         int max = Arrays.stream(arr).max().getAsInt();
