2b3cb8f4b55178a61f0a9f19299d87652d88b138
DWarlock <d.thewarlock@gmail.com>
2020-02-21 09:21:40 +0530

    Flatland Space Station Problem DONE

--- src/com/thewarlock/FlatlandSpaceStation.java
+++ src/com/thewarlock/FlatlandSpaceStation.java
@@ -1,4 +1,27 @@
 package com.thewarlock;
 
+import java.util.Arrays;
+
 public class FlatlandSpaceStation {
+    static int flatlandSpaceStations(int n, int[] c) {
+        Arrays.sort(c);
+        int max=c[0];
+        int dis=0;
+        if (n==c.length)
+            return 0;
+        for(int i=1;i<c.length;i++){
+            dis = (c[i]-(c[i-1]))/2;
+            if(dis > max)
+                max=dis;
+        }
+        dis =(n-1)-(c[c.length-1]);
+        if(dis > max)
+            max=dis;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int c[] = {0,4};
+        System.out.println(flatlandSpaceStations(5, c));
+    }
 }
